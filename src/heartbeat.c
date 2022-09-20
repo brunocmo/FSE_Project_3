75525be153ceedfddc74d866415cf9ed8652932a
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-09-20 18:42:02 -0300

    Add heartbeat to esp32
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

---
+++ src/heartbeat.c
@@ -0,0 +1,80 @@
+#include "heartbeat.h"
+
+static const char *TAG = "ADC EXAMPLE";
+esp_adc_cal_characteristics_t adc1_chars;
+
+void heartbeat_init() {
+    
+    // gpio_config_t io_conf;
+    // io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    // io_conf.mode = GPIO_MODE_OUTPUT;
+    // io_conf.pin_bit_mask = (1ULL << LED);
+    // io_conf.pull_down_en = 0;
+    // io_conf.pull_up_en = 0;
+    // gpio_config(&io_conf);
+
+    // gpio_set_level(LED, 1);
+    // vTaskDelay(1000 / portTICK_RATE_MS);
+    // gpio_set_level(LED, 0);
+    // vTaskDelay(1000 / portTICK_RATE_MS);
+
+    uint32_t voltage;
+    int toVivo = 0;
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_0, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_0));
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+
+
+
+    int incrementador = 0;
+    int minimo = 3000;
+    int maximo = 0;
+
+    int minimoSecond = 3000;
+    int maximoSecond = 0;
+
+    int limit = 0;
+
+    while(1) {
+        voltage = adc1_get_raw(ADC1_CHANNEL_6);
+
+
+        if(voltage > limit) {
+            if(!toVivo) {
+                ESP_LOGI(TAG, "BATIDA!");
+            }
+            toVivo = 1;
+        } else {
+            toVivo = 0;
+        }
+
+        vTaskDelay(50 / portTICK_RATE_MS);
+
+        if (voltage > maximo) maximo = voltage;
+        if (voltage < minimo) minimo = voltage;
+
+        limit = maximo - minimo;
+        limit = (int)((float)(limit) * 0.2);
+        limit = maximo - limit;
+
+        ESP_LOGI(TAG, "valor: %d || %d ", voltage, limit);
+        
+        incrementador++;
+
+
+        if(incrementador > 32 && incrementador < 42 ){
+            if (voltage > maximoSecond) maximoSecond = voltage;
+            if (voltage < minimoSecond) minimoSecond = voltage;
+        }
+
+        if (incrementador>43) {
+            incrementador = 0;
+            maximo = maximoSecond;
+            minimo = minimoSecond;
+            minimoSecond = 3000;
+            maximoSecond = 0;
+        }
+
+    }
+
+}
---
+++ src/heartbeat.h
@@ -0,0 +1,14 @@
+#ifndef HEARTBEAT_H
+#define HEARTBEAT_H
+
+#include <driver/gpio.h>
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "esp_log.h"
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+void heartbeat_init();
+
+#endif
