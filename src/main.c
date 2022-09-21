8a4f4574dc71d31d7efafca5b56946521ab79879
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-09-21 00:24:18 -0300

    Add http and wifi
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/main.c
+++ src/main.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-// Include FreeRTOS for delay
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include "heartbeat.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "freertos/semphr.h"
 
-#define LED 2 // LED connected to GPIO2
+#include "wifi.h"
+#include "http_client.h"
 
-int app_main() {
+xSemaphoreHandle conexaoWifiSemaphore;
 
-    heartbeat_init();
+void RealizaHTTPRequest(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      ESP_LOGI("Main Task", "Realiza HTTP Request");
+      http_request();
+      //https_request();
+    }
+  }
+}
 
-    return 0;
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
 
+    xTaskCreate(&RealizaHTTPRequest,  "Processa HTTP", 4096, NULL, 1, NULL);
+
+
+    
 }
