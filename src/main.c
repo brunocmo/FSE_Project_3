5c0d665693a9c5c052ad38f2e6b524af7e977a4a
Bruno Carmo Nunes <brunocmo.nunes@gmail.com>
2022-09-21 19:19:19 -0300

    Add mqtt with dashboard connection
    Co-authored-by: marcos-mv <vinicius_kof36@hotmail.com>

--- src/main.c
+++ src/main.c
@@ -5,43 +5,77 @@
 #include "esp_http_client.h"
 #include "esp_log.h"
 #include "freertos/semphr.h"
-#include "dht11.h"
+
 
 #include "wifi.h"
 #include "http_client.h"
+#include "mqtt.h"
 
-// xSemaphoreHandle conexaoWifiSemaphore;
+#include "dht11.h"
 
-// void RealizaHTTPRequest(void * params)
-// {
-//   while(true)
-//   {
-//     if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
-//     {
-//       ESP_LOGI("Main Task", "Realiza HTTP Request");
-//       http_request();
-//       //https_request();
-//     }
-//   }
-// }
+xSemaphoreHandle conexaoWifiSemaphore;
+xSemaphoreHandle conexaoMQTTSemaphore;
 
-// void app_main(void)
-// {
-//     // Inicializa o NVS
-//     esp_err_t ret = nvs_flash_init();
-//     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//       ESP_ERROR_CHECK(nvs_flash_erase());
-//       ret = nvs_flash_init();
-//     }
-//     ESP_ERROR_CHECK(ret);
-    
-//     conexaoWifiSemvoid app_main(void) {
-//     semaphore = xSemaphoreCreateBinary();
-//     wifi_start();
+void conectadoWifi(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      // Processamento Internet
+      mqtt_start();
+    }
 
-//     xTaskCreate(&RealizaHTTPRequest,  "Processa HTTP", 4096, NULL, 1, NULL);
-    
-// }
+  }
+
+}
+
+void trataComunicacaoComServidor(void * params)
+{
+  char mensagem[50];
+  char JsonAtriubutos[200];
+  if(xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+  {
+    while(true)
+    {
+       float temperatura = 20.0 + (float)rand()/(float)(RAND_MAX/10.0);
+       sprintf(mensagem, "{\"temperature\": %f}", temperatura);
+       mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+
+       sprintf(JsonAtriubutos, "{\"quantidade de pinos\": 5,\n\"umidade\": 20}");
+       mqtt_envia_mensagem("v1/devices/me/attributes", JsonAtriubutos);
+
+       vTaskDelay(3000 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void app_main(void)
+{
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+
+      ESP_ERROR_CHECK(nvs_flash_erase());
+
+      ret = nvs_flash_init();
+
+    }
+
+    ESP_ERROR_CHECK(ret);
+
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+
+    wifi_start();
+
+    xTaskCreate(&conectadoWifi,  "Conexão ao MQTT", 4096, NULL, 1, NULL);
+
+    xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
+}
+
 
 void pegaTemperatura(void * params) {
     DHT11_init(GPIO_NUM_4);
@@ -51,13 +85,5 @@ void pegaTemperatura(void * params) {
         printf("Status code is %d\n", DHT11_read().status);
         vTaskDelay( 1000 / portTICK_PERIOD_MS);
     }
-
-}
-
-void app_main()
-{
-
-  xTaskCreate(&pegaTemperatura, "Pega temperatura", 4096, NULL, 1, NULL);
-
 
 }
